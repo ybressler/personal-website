be9486006d756c987029d2f8ec14c6c450bd2a34
ybressler <yaakovbressler@gmail.com>
2022-12-28 17:02:17 -0500

    enhanced animations

--- assets/js/fun-hover.js
+++ assets/js/fun-hover.js
@@ -2,29 +2,18 @@ document.addEventListener(
     "DOMContentLoaded",
     function() {
       console.log("Loaded DOM");
-    }
-);
 
+    // Do an animation on page load
+    var $elem = $("section#sparkly-header .fun-hover.headshot");
 
-
-$("section#sparkly-header .fun-hover").on({
-
-  mouseenter: function(){
-    $(this).addClass("on");
-  },
-  mouseleave: function(){
-    setTimeout(() => $(this).removeClass('on') , 500)
-  }
-
-});
-
-$("section#sparkly-header .fun-hover.headshot").on({
-
-  mouseenter: function(){
-    $(this).addClass("on");
-  },
-  mouseleave: function(){
-    setTimeout(() => $(this).removeClass('on') , 500)
-  }
+    $elem.animate({ deg: 720 }, {
+        duration: 1000,
+        step: function (now) {
+            var scale = (1 * now / 720);
+            $(this).css({
+                transform: 'rotate(' + now + 'deg) scale(' + scale + ')'
+            });
+        }
+    });
 
 });
